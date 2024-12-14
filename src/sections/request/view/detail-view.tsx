b11027a75bf2ebb16ed79ff1b8f16599b4f0042b
adrianrivald <adrianrivaldy@gmail.com>
2024-12-14 13:14:36 +0700

    feat: chatbox

--- src/sections/request/view/detail-view.tsx
+++ src/sections/request/view/detail-view.tsx
@@ -9,11 +9,14 @@ import {
   TableCell,
   TableHead,
   TableBody,
+  Input,
 } from '@mui/material';
 
 import { _tasks, _posts, _timeline, _users, _projects } from 'src/_mock';
 import { DashboardContent } from 'src/layouts/dashboard';
 import { StatusBadge } from '../status-badge';
+import { Iconify } from 'src/components/iconify';
+import { SvgColor } from 'src/components/svg-color';
 
 export type ProjectProps = {
   id: string;
@@ -28,7 +31,7 @@ export type ProjectProps = {
 // ----------------------------------------------------------------------
 
 export function RequestDetailView() {
-  const theme = useTheme();
+  const chats = [];
   return (
     <DashboardContent maxWidth="xl">
       <Grid container spacing={3} xs={12}>
@@ -139,7 +142,66 @@ export function RequestDetailView() {
           </Box>
         </Grid>
         <Grid item xs={12} md={4}>
-          Chatbox
+          <Box sx={{ border: 1, borderRadius: 3, borderColor: 'grey.300' }}>
+            <Box
+              p={2}
+              sx={{
+                borderBottom: 1,
+                borderColor: 'grey.300',
+              }}
+            >
+              <Typography>Chat with Sismedika</Typography>
+            </Box>
+            <Box overflow="auto" height={400}>
+              {chats?.length > 0 ? null : (
+                <Box
+                  display="flex"
+                  flexDirection="column"
+                  gap={1}
+                  justifyContent="center"
+                  alignItems="center"
+                  height="100%"
+                  color="grey.500"
+                >
+                  <Box component="img" src="/assets/icons/chat.png" />
+                  <Typography fontWeight="bold">Start a conversation</Typography>
+                  <Typography fontSize={12}>Write something...</Typography>
+                </Box>
+              )}
+            </Box>
+            <Box
+              sx={{
+                borderTop: 1,
+                borderColor: 'grey.300',
+              }}
+              display="flex"
+              alignItems="center"
+              gap={1}
+              justifyContent="space-between"
+            >
+              <Box display="flex" justifyContent="center" alignItems="center" px={2}>
+                <SvgColor src={`/assets/icons/ic-emoji.svg`} />
+              </Box>
+              <Input disableUnderline type="text" />
+              <Box display="flex" alignItems="center" gap={2}>
+                <SvgColor width={18} height={18} src={`/assets/icons/ic-image.svg`} />
+                <SvgColor width={18} height={18} src={`/assets/icons/ic-clip.svg`} />
+                <SvgColor width={18} height={18} src={`/assets/icons/ic-mic.svg`} />
+              </Box>
+              <Box
+                p={2}
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                sx={{
+                  borderLeft: 1,
+                  borderColor: 'grey.300',
+                }}
+              >
+                <SvgColor width={24} height={24} src={`/assets/icons/ic-send.svg`} />
+              </Box>
+            </Box>
+          </Box>
         </Grid>
       </Grid>
     </DashboardContent>
