1d8727588b1cffe60178ba9d2d3905e076e377e0
adrianrivald <adrianrivaldy@gmail.com>
2025-01-08 22:03:02 +0700

    add page and assets

---
+++ src/pages/dashboard/dashboard-internal/index.tsx
@@ -0,0 +1,23 @@
+import { Helmet } from 'react-helmet-async';
+
+import { CONFIG } from 'src/config-global';
+import { DashboardInternalView } from 'src/sections/overview/view/internal-dashboard/dashboard-internal-view';
+
+// ----------------------------------------------------------------------
+
+export default function DashboardInternalPage() {
+  return (
+    <>
+      <Helmet>
+        <title> {`Dashboard - ${CONFIG.appName}`}</title>
+        <meta
+          name="description"
+          content="The starting point for your next project with Minimal UI Kit, built on the newest version of Material-UI ©, ready to be customized to your style"
+        />
+        <meta name="keywords" content="react,material,kit,application,dashboard,admin,template" />
+      </Helmet>
+
+      <DashboardInternalView />
+    </>
+  );
+}
