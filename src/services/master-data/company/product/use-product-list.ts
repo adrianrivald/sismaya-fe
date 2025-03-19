930faa7d1bbfd68c015bb8e2a3da3986bf547712
adrianrivald <adrianrivaldy@gmail.com>
2025-03-19 10:23:19 +0700

    fix product filter

--- src/services/master-data/company/product/use-product-list.ts
+++ src/services/master-data/company/product/use-product-list.ts
@@ -2,10 +2,10 @@ import { useQuery } from "@tanstack/react-query";
 import { http } from "src/utils/http";
 import { Products } from "../types";
 
-export function useProductByCompanyId(companyId: number) {
+export function useProductByCompanyId(companyId: number, isClientCompany?: boolean) {
     return useQuery(['product-items', companyId], async () => {
       const { data: response } = await http<{ data: Products[] }>(
-        `products?company_id=${companyId}`
+        isClientCompany ? `products?client_company_id=${companyId}` : `products?company_id=${companyId}`
       );
   
       return response;
