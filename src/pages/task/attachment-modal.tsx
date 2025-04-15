db822e4c9a621e1e32c204bf7ce38af52babd460
Archimondee <kikchiemedia@gmail.com>
2025-04-15 23:29:00 +0700

    Add zoom in and zoom out

--- src/pages/task/attachment-modal.tsx
+++ src/pages/task/attachment-modal.tsx
@@ -2,6 +2,7 @@ import { Box, Modal, IconButton } from '@mui/material';
 import { Icon } from '@iconify/react';
 import FilePreview from 'src/utils/file-preview';
 import PdfPreview from 'src/utils/pdf-viewer';
+import { TransformWrapper, TransformComponent, useControls } from 'react-zoom-pan-pinch';
 
 interface AttachmentModalProps {
   isOpen: boolean;
@@ -70,28 +71,69 @@ export function AttachmentModal({ isOpen, onClose, url, path }: AttachmentModalP
               maxHeight: '90vh',
               overflow: 'auto',
               borderRadius: 1,
+              justifyContent: 'center',
             }}
           >
-            <IconButton
-              onClick={onClose}
+            <Box
               sx={{
-                position: 'absolute',
-                right: 10,
-                top: 10,
-                color: 'white',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
               }}
             >
-              <Icon icon="solar:close-circle-bold" />
-            </IconButton>
-            <img
-              src={url}
-              alt="attachment"
-              style={{
-                maxWidth: '100%',
-                maxHeight: 'calc(90vh - 32px)',
-                objectFit: 'contain',
-              }}
-            />
+              <TransformWrapper initialScale={1}>
+                {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+                  <>
+                    <TransformComponent>
+                      <img
+                        src={url}
+                        alt="attachment"
+                        style={{
+                          // maxWidth: '100%',
+                          // maxHeight: 'calc(90vh - 32px)',
+                          width: '100%',
+                          height: '100%',
+                          objectFit: 'contain',
+                        }}
+                      />
+                    </TransformComponent>
+                    <IconButton
+                      onClick={() => zoomIn()}
+                      sx={{
+                        position: 'absolute',
+                        right: 40,
+                        bottom: 10,
+                        color: 'black',
+                      }}
+                    >
+                      <Icon icon="gravity-ui:magnifier-plus" />
+                    </IconButton>
+                    <IconButton
+                      onClick={() => zoomOut()}
+                      sx={{
+                        position: 'absolute',
+                        right: 10,
+                        bottom: 10,
+                        color: 'black',
+                      }}
+                    >
+                      <Icon icon="gravity-ui:magnifier-minus" />
+                    </IconButton>
+                    <IconButton
+                      onClick={onClose}
+                      sx={{
+                        position: 'absolute',
+                        right: 10,
+                        top: 10,
+                        color: 'black',
+                      }}
+                    >
+                      <Icon icon="solar:close-circle-bold" />
+                    </IconButton>
+                  </>
+                )}
+              </TransformWrapper>
+            </Box>
           </Box>
         )}
       </Modal>
