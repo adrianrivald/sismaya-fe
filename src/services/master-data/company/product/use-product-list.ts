431e2ee3d5a0f69a9c93a0a2d4787eb27d383509
adrianrivald <adrianrivaldy@gmail.com>
2025-04-29 21:51:23 +0700

    filter product from BE

--- src/services/master-data/company/product/use-product-list.ts
+++ src/services/master-data/company/product/use-product-list.ts
@@ -2,10 +2,11 @@ import { useQuery } from "@tanstack/react-query";
 import { http } from "src/utils/http";
 import { Products } from "../types";
 
-export function useProductByCompanyId(companyId: number, isClientCompany?: boolean, onSuccess?: () => void) {
+export function useProductByCompanyId(companyId: number, isClientCompany?: boolean, onSuccess?: () => void, internalCompanyId?: number) {
+  console.log(internalCompanyId,'internalCompanyId')
     return useQuery(['product-items', companyId], async () => {
       const { data: response } = await http<{ data: Products[] }>(
-        isClientCompany ? `products?client_company_id=${companyId}` : `products?company_id=${companyId}`
+        isClientCompany ? `products?client_company_id=${companyId}${internalCompanyId ? `&company_id=${internalCompanyId}` : ""}` : `products?company_id=${companyId}${internalCompanyId ? `&company_id=${internalCompanyId}` : ""}`
       );
   
       return response;
