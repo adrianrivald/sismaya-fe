c311644ebecc362ac7e5c40e6855d2edde1d2d88
adrianrivald <adrianrivaldy@gmail.com>
2025-05-21 15:31:27 +0700

    fix filter master data

--- src/services/master-data/company/title/use-title-list.ts
+++ src/services/master-data/company/title/use-title-list.ts
@@ -6,16 +6,24 @@ import { usePaginationQuery } from "src/utils/hooks/use-pagination-query";
 
   export function fetchTitleList(params: Partial<any>, company_id?: string) {
     const baseUrl = window.location.origin;
-    const endpointUrl = new URL('/titles?is_active=all', baseUrl);
+    const endpointUrl = new URL('/titles', baseUrl);
   
     if (company_id && params.is_super_admin === false) {
       endpointUrl.searchParams.append('company_id', company_id);
     }
   
+    if (params.company_id) {
+      endpointUrl.searchParams.append('company_id', params.company_id);
+    }
+
     if (params.search) {
       endpointUrl.searchParams.append('search', params.search);
     }
   
+    if (params.is_active) {
+      endpointUrl.searchParams.append('is_active', params.is_active || 'all');
+    }
+    
     dataTableParamsBuilder({
       searchParams: endpointUrl.searchParams,
       ...params,
@@ -25,7 +33,7 @@ import { usePaginationQuery } from "src/utils/hooks/use-pagination-query";
   }
   
   export function useTitleCompanyList(params: Partial<any>, company_id?: string) {
-    return usePaginationQuery(['title-list', params.search, company_id], (paginationState) =>
+    return usePaginationQuery(['title-list', params.search, params.company_id, params.is_active, company_id], (paginationState) =>
       fetchTitleList({ ...params, ...paginationState }, company_id)
     );
   }
