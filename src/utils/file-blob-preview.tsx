5a546b94e795b2933d16516deebfb6d690fc8ae4
Archimondee <kikchiemedia@gmail.com>
2025-06-07 00:22:22 +0700

    🔧 fix(error): fixing error on file-blob-preview.tsx

--- src/utils/file-blob-preview.tsx
+++ src/utils/file-blob-preview.tsx
@@ -1,18 +1,16 @@
 interface Props {
   fileUrl: string;
 }
-const FileBlobPreview = ({ fileUrl }: Props) => {
-  return (
-    <div>
-      <iframe
-        src={`https://docs.google.com/gview?url=${encodeURIComponent(fileUrl)}&embedded=true`}
-        width="100%"
-        height="500px"
-        title="Excel Preview"
-        style={{ border: 'none' }}
-      />
-    </div>
-  );
-};
+const FileBlobPreview = ({ fileUrl }: Props) => (
+  <div>
+    <iframe
+      src={`https://docs.google.com/gview?url=${encodeURIComponent(fileUrl)}&embedded=true`}
+      width="100%"
+      height="500px"
+      title="Excel Preview"
+      style={{ border: 'none' }}
+    />
+  </div>
+);
 
 export default FileBlobPreview;
